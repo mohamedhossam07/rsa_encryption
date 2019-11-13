e9ec65bc6a65467ec4b549b11a87396bb580f19a
mohamedhossam07 <mohamedhossam2012@gmail.com>
2019-11-14 01:10:42 +0200

    Keygen Works

--- keygen.py
+++ keygen.py
@@ -8,66 +8,90 @@ def hcfnaive(a,b):
     else:
         return hcfnaive(b,a%b)
 #
+
 def main():
 
     mlen = 512
     rlen = 512
     leng = 512
-
+    eleng = 1024
     m = rand_bin(leng)
     m =two_com(m,mlen)
 
-    while prime_check(binary2dec(m) ) != True :
+    while isPrime(binary2dec(m) ) != True :
         m = rand_bin(leng)
         m =two_com(m,leng)
 
     r = rand_bin(leng)
     r = two_com(r,leng)
 
-    while prime_check(binary2dec(m) ) != True :
+    while isPrime(binary2dec(r)) != True :
         r = rand_bin(leng)
-        r =two_com(m,mlen)
+        r =two_com(r,eleng)
 
+    e = rand_bin(eleng)
+    e = two_com(e,eleng)
+    #print(e)
+    m = binary2dec(m)
+    r = binary2dec(r)
+    fi = (m-1)*(r-1)
+    g = 5
+    e = binary2dec(e)
+    while g != 1:
+        while isPrime(e) != True :
+            e = rand_bin(eleng)
+            e =two_com(e,eleng)
+            e = binary2dec(e)
+        g = gcd(e, fi)
+
+    #g = gcd(e, fi)
     print('m is = '+str(m))
+    print('')
+    print(isPrime(m))
+    print('')
     print('r is = '+str(r))
-    ilen = mlen + rlen + 1                  #The common length of internal variables
-    a = m + GenZeroStr(rlen + 1)            #A: place M in leftmost position. Fill the left bits with 0.
-    s = TwoComp(m) + GenZeroStr(rlen + 1)   #S: place negative M in leftmost position.
-    p = GenZeroStr(mlen) + r + "0"          #P: place R by rightmost 0.
+    print('')
+    print(isPrime(r))
+    print('')
+    print('e is = '+str(e))
+    print(isPrime(e))
+    print('')
 
-    for i in range(rlen):
 
-        op = p[-2:]
-        if   op == "10":
-            p = BitAdd(p, s, len(p))
-        elif op == "01":
-            p = BitAdd(p, a, len(p))
-        p = BitShift(p, 1)
+    d = multiplicative_inverse(e, fi)
+    print(d)
 
-    p = p[:-1]
-    print("The answer is: " + str(binary2dec(p)) )
 
-def prime_check(n,k=5):
-    r =randint(2, n)
-    if hcfnaive(n,r) != 1 :
-        return False
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
     else:
-        if n < 2: return False
-        for p in [2,3,5,7,11,13,17,19,23,29]:
-            if n % p == 0: return n == p
-        s, d = 0, n-1
-        while d % 2 == 0:
-            s, d = s+1, d/2
-        for i in range(k):
-            x = pow(randint(2, n-1), d)
-            if x == 1 or x == n-1: continue
-            for r in range(1, s):
-                x = (x * x) % n
-                if x == 1: return False
-                if x == n-1: break
-            else: return False
-        return True
+        g, x, y = egcd(b % a, a)
+        return (g, y - (b // a) * x, x)
 
+
+def isPrime(n, k=5): # miller-rabin
+    from random import randint
+    if n < 2: return False
+    for p in [2,3,5,7,11,13,17,19,23,29]:
+        if n % p == 0: return n == p
+    s, d = 0, n-1
+    while d % 2 == 0:
+        s, d = s+1, d/2
+    for i in range(k):
+        x = pow(randint(2, n-1), d, n)
+        if x == 1 or x == n-1: continue
+        for r in range(1, s):
+            x = (x * x) % n
+            if x == 1: return False
+            if x == n-1: break
+        else: return False
+    return True
+
+def gcd(a, b):
+    while b != 0:
+        a, b = b, a % b
+    return a
 
 
 if __name__ == "__main__":
--- keygen_extras.py
+++ keygen_extras.py
@@ -59,3 +59,30 @@ def rand_bin(leng):
         #print (out)
     out = size_be_R(out,leng)
     return binary2dec(out)
+
+
+
+def multiplicative_inverse(a, b):
+    """Returns a tuple (r, i, j) such that r = gcd(a, b) = ia + jb
+    """
+    # r = gcd(a,b) i = multiplicitive inverse of a mod b
+    #      or      j = multiplicitive inverse of b mod a
+    # Neg return values for i or j are made positive mod b or a respectively
+    # Iterateive Version is faster and uses much less stack space
+    x = 0
+    y = 1
+    lx = 1
+    ly = 0
+    oa = a  # Remember original a/b to remove
+    ob = b  # negative values from return results
+    while b != 0:
+        q = a // b
+        (a, b) = (b, a % b)
+        (x, lx) = ((lx - (q * x)), x)
+        (y, ly) = ((ly - (q * y)), y)
+    if lx < 0:
+        lx += ob  # If neg wrap modulo orignal b
+    if ly < 0:
+        ly += oa  # If neg wrap modulo orignal a
+    # return a , lx, ly  # Return only positive values
+    return lx
